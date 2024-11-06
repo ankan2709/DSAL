b9b35c2b71a1845f36554069a511d0390281edab
ankan2709 <ankandash18@gmail.com>
2024-11-06 09:23:57 -0500

    small update

--- leetcode/subarraySumlengthK.py
+++ leetcode/subarraySumlengthK.py
@@ -1,12 +1,14 @@
 def subarray_fixed_sum(nums, k):
 
-    curr_sum = sum(nums[:k])
-
+    max_sum = sum(nums[:k])
+    curr_sum = max_sum
     for right in range(k, len(nums)):
         left = right - k
-        curr_sum = max(curr_sum, sum(nums[left:right]))
+        curr_sum -= nums[left]
+        curr_sum += nums[right]
+        max_sum = max(max_sum, curr_sum)
 
-    return curr_sum
+    return max_sum
 
 nums = [1, 2, 3, 7, 4, 1]
 k = 3

6e250558d30ba94860d2150e00b1c5f7efaa462e
ankan2709 <ankandash18@gmail.com>
2024-11-08 18:04:30 -0500

    encode decode string

---
+++ leetcode/encodeDecodeString.py
@@ -0,0 +1,14 @@
+def encode(listString):
+    val = " ".join(listString)
+
+
+    return val
+
+def decode(string):
+    return string.split(" ")
+
+
+# print(encode(["Hello","World"]))
+# print(decode("Hello World"))
+
+print(encode(["","4 "]))

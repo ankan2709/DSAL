940f6373bf0c21c8c00fe64fb6ae58e1fda01a71
ankan2709 <ankandash18@gmail.com>
2024-11-07 20:34:47 -0500

    depth of BT

---
+++ leetcode/depthOfBinaryTree.py
@@ -0,0 +1,8 @@
+def dfs(root):
+    if not root:
+        return 0
+    
+    left = dfs(root.left)
+    right = dfs(root.right)
+
+    return 1 + max(left, right)
--- leetcode/fibonacci.py
+++ leetcode/fibonacci.py
@@ -10,4 +10,13 @@ def fibonacci(n):
     memo[n] = fibonacci(n-1) + fibonacci(n-2)
     return memo[n]
     
-print(fibonacci(30))
+# print(fibonacci(30))
+
+
+dp = [0, 1]
+def fiboDPbottomUp(n):
+    for i in range(2, n+1):
+        dp.append(dp[i-1] + dp[i-2])
+
+    return dp[-1]
+print(fiboDPbottomUp(5))

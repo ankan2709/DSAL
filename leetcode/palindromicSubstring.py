c89763bdef1ddf28dd70f70ac63f140973e9fca1
ankan2709 <ankandash18@gmail.com>
2024-12-11 11:03:10 -0500

    palindromic substring brute force index

--- leetcode/palindromicSubstring.py
+++ leetcode/palindromicSubstring.py
@@ -1,38 +1,33 @@
 
 
 
+
+
+
+
+
+# # brute force
+
+def isPalidrome(s):
+    l = 0
+    r = len(s) - 1
+    while l <= r:
+        if s[l] != s[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
 def palindrominSubstringIndex(s):
     count = 0
     for i in range(len(s)):
         for j in range(i+1, len(s)+1):
             subString = s[i:j]
-            if subString == subString[::-1]:
+            if isPalidrome(subString):
                 count += 1
     return count
 
-print(palindrominSubstringIndex('aaa'))
-
-# # brute force
-# def getSubstring(string):
-#     res = []
-
-#     for i in range(len(string)):
-#         for j in range(i+1, len(string)+1):
-#             res.append(string[i:j])
-
-#     return res
-
-# def isPalindrome(string):
-#     return string == string[::-1]
+print(palindrominSubstringIndex('abc'))
 
 
-# def palindrominSubstring(string):
-#     res = getSubstring(string)
 
-#     number = 0
-#     for item in res:
-#         if isPalindrome(item):
-#             number += 1
-#     return number
-
-# print(palindrominSubstring('abc'))
